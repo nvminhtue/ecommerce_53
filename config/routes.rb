6bb3221934a9ffc0c7f00a155219d9b48764b68e
Nguyen Viet Minh Tue <nvminhtue@gmail.com>
2018-09-27 21:27:20 +0700

    Signup/Login/Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+
+  get "sessions/new"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   resources :products
   resources :users
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Minh Tue",
+  email: "nvminhtue@gmail.com",
+  address: "16 Ly Thuong Kiet",
+  phone: "0905507959",
+  password: "123123",
+  password_confirmation: "123123",
+  role: 1)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  address = "16 Ly Thuong Kiet"
+  phone = "0905507959"
+
+  User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    address: address,
+    phone: phone)
+end

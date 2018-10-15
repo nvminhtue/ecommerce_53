34a607b389f9cc8b9dc830438125bdeca6dad06a
Nguyen Viet Minh Tue <nvminhtue@gmail.com>
2018-10-15 20:34:13 +0700

    Test Catagory

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -6,4 +6,14 @@ module ProductsHelper
   def image_for product
     image_tag product.picture.url
   end
+
+  # def nested_categories category
+  #   category.map do |parent|
+  #     # if parent.children.nil?
+  #     #   content_tag(:div, parent.name)
+  #     # else
+  #     parent.name + content_tag(:div, nested_categories(parent.children))+ "---"
+  #     # content_tag(:div, parent.name) + content_tag(:div, nested_categories(parent.children))
+  #   end
+  # end
 end
